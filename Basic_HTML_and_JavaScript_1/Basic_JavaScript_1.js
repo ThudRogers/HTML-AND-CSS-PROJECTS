16302bfc685a8dc2abcefa9c7cfdec39f98335e2
ThudRogers <josephmleiter@gmail.com>
2025-08-04 09:07:37 -0400

    Adding Basic_HTML_and_JavaScript_1 to my HTML folder
    Added all of the projects for basic html and java script to my main html class repository

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,9 @@
+
+function My_First_Function() {
+	var String = "Kiss me, I'm Irish!";
+	var result = String.fontcolor("green"); 
+	document.getElementById("Irish").innerHTML = 
+	result; 	
+}
+
+alert("I have to let you know that Goblins rule");
