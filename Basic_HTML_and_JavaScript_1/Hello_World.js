9e914ef763bb4bc50cef693de3ebaaf5e6a65ef0
ThudRogers <josephmleiter@gmail.com>
2025-08-05 22:48:21 -0400

    added
    added stuff to helloworld javascript and another sentence to TESTINGANOTHERHTML

--- Basic_HTML_and_JavaScript_1/Hello_World.js
+++ Basic_HTML_and_JavaScript_1/Hello_World.js
@@ -1,1 +1,8 @@
-alert("Hello, World! I am an alert box!");
+var greeting = "Hello, World!";
+console.log(greeting);
+
+var greeting2 = 'Welcome to Node.js!';
+console.log(greeting2);
+
+var sentence1 = 'Learning how to debug code with the debugger.';
+console.log(sentence1);
